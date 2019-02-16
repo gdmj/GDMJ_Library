ae52f5a78f67bc2fdd673c773fa98d3be25bef30
MrDenius <denis_ovchinikov03@mail.ru>
2019-02-16 16:40:00 +1000

    Добавлен class LogBox

---
+++ GDMJ_Library/Class/LogBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDMJ_Library.Class
+{
+    class LogBox
+    {
+        private ListBox LBox;
+
+        public LogBox(ListBox _LBox)
+        {
+            LBox = _LBox;
+        }
+
+        public void Log(String _Text)
+        {
+            LBox.Items.Add(_Text);
+        }
+    }
+}
