daf493205f4d3549b0d52d598ea7e27848b7d642
Glipko <glipkoclashroyale@gmail.com>
2019-02-17 19:31:44 +1000

    XmlEdito Добавления нового тега

---
+++ GDMJ_Library/Class/XmlEditor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Xml;
+
+namespace GDMJ_Library.Class
+{
+    public class XmlEditor
+    {
+
+        private String path;
+
+        public XmlEditor(String path)
+        {
+            this.path = path;
+        }
+
+        public void AddNewNode(ArrayList newNodeInfo)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            XmlElement head = doc.DocumentElement;
+            XmlElement newNode = doc.CreateElement(newNodeInfo[0].ToString());
+            for (int i = 1; i < newNodeInfo.Count; i += 2)
+            {
+                LogBox log = new LogBox();
+                XmlElement childNode = doc.CreateElement(newNodeInfo[i].ToString());
+                XmlText childValue = doc.CreateTextNode(newNodeInfo[i].ToString());
+                childNode.AppendChild(childValue);
+                newNode.AppendChild(childNode);
+            }
+            head.AppendChild(newNode);
+            doc.Save(path);
+        }
+    }
+}
--- GDMJ_Library/DebugForm.cs
+++ GDMJ_Library/DebugForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GDMJ_Library.Class;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
